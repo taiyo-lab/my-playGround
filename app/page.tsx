168e5f4eb07cca744bd83f9bc794fd224ba4218e
小林大洋 <koba.syyukied@gmail.com>
2024-04-07 16:42:30 +0900

    useState試したいけどエラー吐かれる。

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,16 @@
-export default function Home() {
-  return <div>test</div>;
-}
+// page.tsx
+// use client
+import { useState } from "react";
+
+const page = () => {
+  // const [count, setCount] = useState(0);
+
+  return (
+    <div>
+      <p>Count</p>
+      <button>Increment</button>
+    </div>
+  );
+};
+
+export default page;
